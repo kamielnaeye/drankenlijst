37c37eabe108d86fc4becc9c88c952b5f680a647
kamielnaeye <c0405572@student.viveshbo5.be>
2019-06-13 16:19:32 +0200

    Drankenlijst met foto's socket io werkte niet

--- app.js
+++ app.js
@@ -5,26 +5,32 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var app = express();
 var mongoose = require('mongoose');
-var tekstSchema = new mongoose.Schema({naam:String,post:String});
-var Tekst = mongoose.model('tekst',tekstSchema);
+var drankSchema = new mongoose.Schema({naam:String,prijs:String,url:String});
+var Drank = mongoose.model('drank',drankSchema);
+var http = require('http').Server(app);
+var io = require('socket.io');
+const port = 2000;
+const socket = io(http);
 var arrNaam = [];
-var arrPost = [];
+var arrPrijs = [];
+var arrURL = [];
 // view engine setup
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,'node_modules/socket.io-client/dist/')));
 app.listen(process.env.PORT || 3000 , process.env.IP || '0.0.0.0');
 //connectie met database
-mongoose.connect("mongodb+srv://admin:Project123@projectkk-qrdxb.azure.mongodb.net/tekstform?retryWrites=true", function(err) {
+mongoose.connect("mongodb+srv://admin:Project123@projectkk-qrdxb.azure.mongodb.net/dranken?retryWrites=true", function(err) {
     if (err) throw err;
     console.log("Successfully connected to mongodb");
   });
 //post
 app.post('/post', function(req, res){
   delete req.body._id;
-  var tekstNew = new Tekst(req.body);
-  tekstNew.save(function(err){
+  var drankNew = new Drank(req.body);
+  drankNew.save(function(err){
     if(err)throw err;
       console.log("post saved!!");
   })
@@ -35,15 +41,28 @@ app.post('/post', function(req, res){
 });
 //get
 app.get('/list',function(req,res){
- Tekst.find(null, function(err,docs){
+ Drank.find(null, function(err,docs){
   if(err)throw err;
   arrNaam = [];
-  arrPost = [];
+  arrPrijs = [];
+  arrURL = [];
   for(var i = 0;i<docs.length;i++){
     arrNaam.push(docs[i].naam);
-    arrPost.push(docs[i].post)
+    arrPrijs.push(docs[i].prijs);
+    arrURL.push(docs[i].url);
   }
-  res.render('lijst',{naam: arrNaam, post: arrPost, title:"lijst"});
+  res.render('lijst',{naam: arrNaam, prijs: arrPrijs, url: arrURL, title:"lijst"});
  });
+});
+
+http.listen(port, ()=>{
+  console.log("connected to port: "+ port)
+  });
+socket.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('chat message', function(msg){
+      console.log('message: ' + msg);
+      socket.emit('chat message', msg);
+    });
 });
 module.exports = app;
